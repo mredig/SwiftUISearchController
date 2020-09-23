116a24ac3d24a8cbf4923af67a2ef81fe23faeb6
Michael Redig <mredig@gmail.com>
2020-09-23 11:40:10 -0500

    initial commit

---
+++ SwiftUISearchController/SwiftUISearchControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISearchControllerApp.swift
+//  SwiftUISearchController
+//
+//  Created by Michael Redig on 9/23/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISearchControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
